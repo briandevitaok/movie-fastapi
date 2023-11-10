dff8b4d3333dd64130ec6163c7a7c539eb442ad1
unknown <16tupac16@gmail.com>
2023-11-09 21:40:51 -0300

    Despliegue

--- main.py
+++ main.py
@@ -25,3 +25,6 @@ Base.metadata.create_all(bind=engine)
 def read_root():
     return HTMLResponse('<h2> Hola mundo! </h2>')
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
