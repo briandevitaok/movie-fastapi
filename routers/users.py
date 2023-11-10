8bee400b0f50cc7336b2aaf9c1bac4c9adaa702d
unknown <16tupac16@gmail.com>
2023-11-09 21:07:55 -0300

    Despliegue

---
+++ routers/users.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from pydantic import BaseModel 
+from fastapi.responses import HTMLResponse,JSONResponse
+from user_jwt import createToken,validateToken
+login_user = APIRouter()
+
+class User(BaseModel):
+    email: str 
+    password: str
+
+
+
+@login_user.post('/login', tags=['authentication'])
+def login(user: User):
+    if user.email == 'briandevita16@gmail.com' and user.password == '123':
+        token: str = createToken(user.dict())
+        print(token)
+        return JSONResponse(content=token)
