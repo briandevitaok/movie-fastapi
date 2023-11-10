0ce180c982f13807fe4882dc8cf6a8ccd788266e
unknown <16tupac16@gmail.com>
2023-11-10 10:48:07 -0300

    Despliegue

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse,JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse  # Update this line
 from pydantic import BaseModel
 from bd.database import engine, Base
 from routers.movie import routerMovie
 from routers.users import login_user
-
+import os
+import uvicorn
 
 app = FastAPI(
     title='Aprendiendo FastApi',
@@ -15,16 +16,14 @@ app = FastAPI(
 app.include_router(routerMovie)
 app.include_router(login_user)
 
-
-
 Base.metadata.create_all(bind=engine)
-
-
 
 @app.get('/', tags=['inicio'])
 def read_root():
     return HTMLResponse('<h2> Hola mundo! </h2>')
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0",
-                port=int(os.environ.get("PORT", 8000)))
+    # Obtén el puerto de la variable de entorno o utiliza 8000 como valor predeterminado
+    port = int(os.environ.get("PORT", 8000))
+    
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
