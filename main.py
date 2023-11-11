b3c89a1d0617447d46b8473f454d3b98d480e411
unknown <16tupac16@gmail.com>
2023-11-10 21:55:24 -0300

    Despliegue

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ def read_root():
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
-    uvicorn.run('main:app', host:"0.0.0.0", port=port)
+
+
+    uvicorn.run("main:app", host:"0.0.0.0", port=port)
