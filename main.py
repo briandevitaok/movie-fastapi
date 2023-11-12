aa6084ddd8f52633cbff4ea719c9113a5fcc997a
unknown <16tupac16@gmail.com>
2023-11-12 19:22:32 -0300

    Despliegue

--- main.py
+++ main.py
@@ -23,9 +23,6 @@ def read_root():
     return HTMLResponse('<h2> Hola mundo! </h2>')
 
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
-
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
